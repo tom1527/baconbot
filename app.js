56f5f9f9f24782dc75d8189bfa54066bbc48341f
tom1527 <tom.bradley@webcurl.co.uk>
2022-05-19 11:07:43 +0100

    Improve error handling (for deffered)

--- app.js
+++ app.js
@@ -70,7 +70,11 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		if(interaction.isRepliable()) {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	}
 });
 
