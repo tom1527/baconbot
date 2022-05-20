922d4079cb8925269510f1220e53d5d3ae368df8
tom <twbradley@hotmail.com>
2022-05-20 19:06:51 +0100

    Clean up repo

--- commands/random/woke.js
+++ commands/random/woke.js
@@ -1,43 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { GuildMemberManager, MessageEmbed } from 'discord.js';
 
-/*
-    async run(message, args){
-        
-        let mColl = await message.channel.fetchMessages({limit: 2});
-        let mArray = await mColl.array();
-        let re = / /g;
-        let str = "";
-        let person;
-
-        if(!mArray[1].author.bot){
-            if(mArray[1].content.includes(" ")){
-                str = await mArray[1].content.replace(re, " :clap: ");
-                str = ":clap: " + str + " :clap:";
-            }
-            else str = ":clap: " + mArray[1].content + " :clap:";
-        }
-        else str = ":clap: fuck :clap: you :clap:"; 
-
-        message.channel.send({embed: {
-            color: 0xd65d00,
-            author: {
-                name: mArray[1].member.nickname,
-                icon_url: mArray[1].author.avatarURL
-            },
-            description: str
-        }});
-
-        //message.channel.send("Hold on a fucking day or two, goddamn.");
-        
-    }
-<<<<<<< HEAD
-}
-
-module.exports = Woke;
-=======
-} */
-
 const execute = async function execute(interaction) {
     let mColl = await interaction.channel.messages.fetch({limit: 2});
     let mArray = await Array.from(mColl);
@@ -85,4 +48,3 @@ async function create() {
 }
 
 export { create }
->>>>>>> refactor
--- index.js
+++ index.js
@@ -1,42 +1,85 @@
 #!/usr/bin/env node 
-const commando = require('discord.js-commando');
-const fs = require('fs');
-const passiveCommands = require('./commands/bot/passiveCommands');
-const msg = require('./commands/bot/msg');
+import *  as fs from 'fs';
+import * as path from 'path';
+import 'dotenv/config';
+import {Client, Collection, Intents} from 'discord.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import * as passiveCommands from './commands/bot/passiveCommands.js';
+import * as msg from './commands/bot/msg.js';
+import Glob from 'glob'
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// auth file (saves from hard-coding keys)
-const filePath = "discordauth.json";
-const auth = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// create bot user
-const bot = new commando.Client({owner: auth.owner_id, unknownCommandResponse: false, nonCommandEditable: false});
-bot.login(auth.bot_id);
-const guildID = auth.guild_id;
+const guildID = process.env.GUILD_ID;
+client.commands = new Collection();
+const commandGlob = Glob.sync('commands' + '/**/*');
+const commandFiles = commandGlob.filter(file => file.endsWith('.js'));
 
-// register commands
-bot.registry.registerGroup('dictionary', 'Dictionary');
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerGroup('misc', 'Misc');
-bot.registry.registerGroup('dev', 'Dev');
-bot.registry.registerCommandsIn(__dirname + "/commands");
+for (const file of commandFiles) {
+	if (file.includes('commands/bot/')) {
+		continue;
+	}
+	var command = await import(`./${file}`);
+	try {
+		command = await command.create();
+	} catch (error) {
+		console.log(error);
+	}
+	(async () => {
+		try {
+			client.commands.set(command.data.name, command);
+			console.log(`Created ${command.data.name}`);
+		} catch (error) {
+			console.log(error);
+		}
+	})();
+}
 
-bot.on('error', function(){
-    console.error();
+
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-bot.on('message', function(message){
-    if(message.channel.id != "694914362095304771") // blacklist code channel from collecting source data for !markov command
-        msg.appendMessage(message);
+client.on('messageCreate', (message) => {
+	if(message.channel.id != "694914362095304771") { // blacklist code channel from collecting source data for !markov command
+		msg.appendMessage(message);
+	}
 });
 
-// get latest channel pin by going through all the channels, grabbing the last pin of each channel, selecting the one with the latest date, getting the channel from that.
 let currentChannelID;
-bot.on('ready', function(){
-    guild = bot.guilds.get(guildID);
-    currentChannelID = passiveCommands.currentChannelID(guild);
-});
+client.on('ready', async function(client){
+	const guild = await client.guilds.fetch(guildID);
+	currentChannelID = passiveCommands.currentChannelID(guild, client);
+})
 
-bot.on('channelPinsUpdate', async function(channel, time){
+client.on('channelPinsUpdate', async function(channel, time){
     currentChannelID = channel.id;
     await passiveCommands.loadPins(channel);
 });
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		try {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: false });
+		}	catch (error) {
+			if (error.code === 'INTERACTION_ALREADY_REPLIED') {
+				await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: false });
+			} else {
+				console.log(error);
+			}
+		}
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
