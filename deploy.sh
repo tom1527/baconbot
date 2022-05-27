bdeb60003c8943f6394c7767ec58cdb22dc3302f
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-05-28 00:50:41 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Copy service file, incase if there are any changes
-sudo cp baconbot.service /etc/systemd/system/baconbot.service
+sudo cp baconbot2.service /etc/systemd/system/baconbot2.service
 
 # reload configurations incase if service file has changed
 sudo systemctl daemon-reload
