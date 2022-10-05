06366fefe5677c95286d08695eee62efd5898f48
Oracle Public Cloud User <opc@bacon-bot.subnet03191551.vcn03191551.oraclevcn.com>
2022-10-05 17:30:57 +0000

    Changes to package for voice

--- unregister-commands.js
+++ unregister-commands.js
@@ -5,13 +5,14 @@ import 'dotenv/config';
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.get(Routes.applicationCommands(process.env.APP_ID,  process.env.GUILD_ID))
+rest.get(Routes.applicationGuildCommands(process.env.APP_ID,  process.env.GUILD_ID))
 	.then(data => {
 		const promises = [];
 		for (const command of data) {
             console.log(command.name);
             console.log(command.id);
-            const deleteUrl = `${Routes.applicationCommands(process.env.APP_ID)}/${command.id}`;
+            // const deleteUrl = `${Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID)}/${command.id}`;
+		const deleteUrl = `${Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID)}/${command.id}`;
 			promises.push(rest.delete(deleteUrl));
 		}
 		return Promise.all(promises).then(console.log(`Deleting ${promises.length} commands`));
