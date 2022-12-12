6686acb37c32e7c23a072d3a4dc25e309ab1f955
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-12-12 15:15:51 +0000

    Remove unused deploy bash script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Copy service file, incase if there are any changes
-sudo cp baconbot2.service /etc/systemd/system/baconbot2.service
-
-# reload configurations incase if service file has changed
-sudo systemctl daemon-reload
