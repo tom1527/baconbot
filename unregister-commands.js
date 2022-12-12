b4273faa1822b9c0863254ab7f1e12e92740c7e3
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-12-12 15:34:27 +0000

    Better error handling for unregister-commands.js

--- unregister-commands.js
+++ unregister-commands.js
@@ -2,18 +2,22 @@ import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
 import 'dotenv/config';
 
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+if (!process.env.APP_ID || !process.env.GUILD_ID || !process.env.DISCORD_TOKEN) {
+	console.log('Environment variables missing, commands cannot be deployed.');
+} else {
+	const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.get(Routes.applicationGuildCommands(process.env.APP_ID,  process.env.GUILD_ID))
-	.then(data => {
-		const promises = [];
-		for (const command of data) {
-            console.log(command.name);
-            console.log(command.id);
-            // const deleteUrl = `${Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID)}/${command.id}`;
-		const deleteUrl = `${Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID)}/${command.id}`;
-			promises.push(rest.delete(deleteUrl));
-		}
-		return Promise.all(promises).then(console.log(`Deleting ${promises.length} commands`));
-	})
-	.catch(console.error);
+	rest.get(Routes.applicationGuildCommands(process.env.APP_ID,  process.env.GUILD_ID))
+		.then(data => {
+			const promises = [];
+			for (const command of data) {
+		    console.log(command.name);
+		    console.log(command.id);
+		    // const deleteUrl = `${Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID)}/${command.id}`;
+			const deleteUrl = `${Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID)}/${command.id}`;
+				promises.push(rest.delete(deleteUrl));
+			}
+			return Promise.all(promises).then(console.log(`Deleting ${promises.length} commands`));
+		})
+		.catch(console.error);
+}
