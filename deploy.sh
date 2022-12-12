348408b0ccce491a32d29f33e1ed36d237a812e7
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-12-12 15:23:46 +0000

    Rename service in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Copy service file, incase if there are any changes
-sudo cp baconbot2.service /etc/systemd/system/baconbot2.service
+sudo cp porkbot.service /etc/systemd/system/porkbot.service
 
 # reload configurations incase if service file has changed
 sudo systemctl daemon-reload
