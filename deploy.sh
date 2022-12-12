9ed1d9e61505104e76c0f4e88be466ef76773e8c
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-12-12 16:05:40 +0000

    Remove unnecessary deployment bash script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Copy service file, incase if there are any changes
-sudo cp porkbot.service /etc/systemd/system/porkbot.service
-
-# reload configurations incase if service file has changed
-sudo systemctl daemon-reload
