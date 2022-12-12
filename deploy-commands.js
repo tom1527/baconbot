183212c4c801203271a59abc60cf7c959b91104e
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-12-12 15:15:18 +0000

    Better error handling for deploy-commands script

--- deploy-commands.js
+++ deploy-commands.js
@@ -20,9 +20,12 @@ for (const file of commandFiles) {
 	}
 }
 
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+if (!process.env.APP_ID || !process.env.GUILD_ID || !process.env.DISCORD_TOKEN) {
+	console.log("Environment variables missing, commands cannot be deployed.);
+else {
+	const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationCommands(process.env.APP_ID,  process.env.GUILD_ID), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
-	
+	rest.put(Routes.applicationCommands(process.env.APP_ID,  process.env.GUILD_ID), { body: commands })
+		.then(() => console.log('Successfully registered application commands.'))
+		.catch(console.error);	
+}
